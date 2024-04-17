25951597a5d0f8831b9be77388ada551a23a4eb8
BeTaJI <lilysh994@mail.ru>
2024-04-17 23:05:37 +0300

    проверка гихтабпеджес из рут папки

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/forum",
-  build: {
-    outDir: "docs",
-  },
+  // build: {
+  //   outDir: "docs",
+  // },
   plugins: [react()],
 });
